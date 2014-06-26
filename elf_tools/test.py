e804890a81986fabf19da0e9f9694ba42b3ddbc8
buaakq <kkqq666666@163.com>
2014-06-26 18:59:02 +0800

    get the relocation value

--- elf_tools/test.py
+++ elf_tools/test.py
@@ -9,9 +9,38 @@ from elfpatch import elfpatch
 from elf_helper import *
 from genshellcode import *
 
+def relocate(rel):
+    for k in rel:
+      k['result'] = -4 + k['sym_addr'] - k['rel_addr']
+      k.pop('sym_addr')
+      k.pop('rel_addr')
+    return rel 
+
+def get_sym_addr(rel,vxworks):
+    syms = parse_text_section(vxworks)
+    for k in rel:
+      sym_name = k['sym_name']
+      if sym_name in syms:
+        k['sym_addr'] =int(syms[sym_name]['offset'],16)
+        k.pop('sym_name')
+    return rel
+    
+
+def get_func_addr(rel,func):
+    inject_point = '3d6186'
+    for t in rel:
+      func_name = t['func_name']
+      if func_name in func:
+        inject_offset = int(func[func_name]['offset'],16)
+        func_addr = int(inject_point,16) + inject_offset
+        t['rel_addr'] = func_addr + t['offset2func']
+        t.pop('offset2func')
+        t.pop('func_name')
+    return rel 
+
 def modify_rel_table(rel,sym):
     sym_reverse = sym[::-1]
-    print sym_reverse
+    #print sym_reverse
     for t in rel:
       rel_off = t['rel_offset']
       #print rel_off
@@ -58,13 +87,22 @@ def main():
                       dest="OBJECT_FILE", action="store",
                       help="Get text section and  add it to out file    ")
 
+    parser.add_option("-f","--file",dest="FILE",action=
+                    "store", type="string")
     (options, args) = parser.parse_args()
 
     func = parse_text_section(options.OBJECT_FILE)
     rel_table = get_relocate_text(options.OBJECT_FILE)
     sym_table = get_symtab_text(options.OBJECT_FILE)
     rel_table = modify_rel_table(rel_table, sym_table)
-
+    
+    funcs = parse_text_section(options.OBJECT_FILE)
+    rel_table = get_func_addr(rel_table,funcs)
+    rel_table  = get_sym_addr(rel_table,options.FILE)  
+    
+    rel_table = relocate(rel_table) 
+     
+    #print funcs
 
     #print func
     #print sym_table   
